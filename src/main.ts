6f775a7016b4c252d03827cfaf72f306f43e5d97
Georgi Yolovski <bluef0rever@abv.bg>
2017-05-20 13:54:37 +0300

    Added a starter project with Vue and TS

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,15 @@
-import * as Vue from 'vue'
-import * as VueRouter from 'vue-router'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-import './vendor' //bootstrap
-require('./main.scss'); //global css
+Vue.config.productionTip = false
 
-/*
-  For components that will be used in html (such as navbar),
-  all you need to do is import the file somewhere in your code,
-  they are automatically registered when the file is loaded.
-  However, if you import the class (ex: import { Navbar } from './navbar'),
-  you will have to call new Navbar() somewhere as well. You would want
-  to do that if you are defining a components{} object in the @VueComponent
-  options parameter. 
-*/
-import './components/navbar/navbar'
-
-import { HomeComponent } from './views/home/home'
-import { AboutComponent } from './views/about/about'
-
-Vue.use(VueRouter);
-
-var app = Vue.extend({});
-
-var router = new VueRouter();
-
-router.map({
-  '/' : {
-      component: HomeComponent
-  },
-  '/about' : {
-      component: AboutComponent
-  }
-});
-
-router.start(app, '#app-main');
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
