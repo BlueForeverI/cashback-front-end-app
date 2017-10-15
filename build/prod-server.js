2cdf2d77d86360b1090d851ffae9ea97cc40853b
Georgi Yolovski <blueforeverabv@gmail.com>
2017-10-15 21:06:44 +0300

    Fixed production config

---
+++ build/prod-server.js
@@ -0,0 +1,70 @@
+require('./check-versions')()
+
+var config = require('../config')
+if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = "production"
+}
+
+var opn = require('opn')
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
+var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = require('./webpack.prod.conf');
+
+// default port where prod server listens for incoming traffic
+var port = process.env.PORT || 4000
+var app = express()
+var compiler = webpack(webpackConfig)
+
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    quiet: true
+})
+
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
+        log: () => {}
+    })
+    // force page reload when html-webpack-plugin template changes
+compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+        hotMiddleware.publish({ action: 'reload' })
+        cb()
+    })
+})
+
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')())
+
+// serve webpack bundle output
+app.use(devMiddleware)
+
+// enable hot-reload and state-preserving
+// compilation error display
+app.use(hotMiddleware)
+
+// serve pure static assets
+var staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
+app.use(staticPath, express.static('./static'))
+
+var uri = 'http://localhost:' + port
+
+var _resolve
+var readyPromise = new Promise(resolve => {
+    _resolve = resolve
+})
+
+console.log('> Starting prod server...')
+devMiddleware.waitUntilValid(() => {
+    console.log('> Listening at ' + uri + '\n')
+    _resolve()
+})
+
+var server = app.listen(port)
+
+module.exports = {
+    ready: readyPromise,
+    close: () => {
+        server.close()
+    }
+}
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,10 @@
-module.exports = {
-  NODE_ENV: '"production"'
-}
+var merge = require('webpack-merge')
+
+module.exports = merge({}, {
+    NODE_ENV: '"production"',
+    API_URL: `"${process.env.CASHBACK_API_URL}"`,
+    OAUTH_GRANT_TYPE: '"password"',
+    OAUTH_CLIENT_ID: `"${process.env.CASHBACK_OAUTH_CLIENT_ID}"`,
+    OAUTH_CLIENT_SECRET: `"${process.env.CASHBACK_OAUTH_CLIENT_SECRET}"`,
+    OAUTH_SCOPE: '"read,write"',
+})
