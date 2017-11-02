592f8f13219fdc5acf14c04418a8cdb39cb98f23
Georgi Yolovski <blueforeverabv@gmail.com>
2017-11-02 23:05:55 +0200

    Reverted dev API URL to localhost

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,7 @@ var merge = require('webpack-merge')
 
 module.exports = merge({}, {
     NODE_ENV: '"dev"',
-    API_URL: '"https://cashback-back-end.herokuapp.com"',
+    API_URL: '"http://localhost:8080"',
     OAUTH_GRANT_TYPE: '"password"',
     OAUTH_CLIENT_ID: '"cashback-private-content"',
     OAUTH_CLIENT_SECRET: '"NV2MfFAP4drPaHBC"',
